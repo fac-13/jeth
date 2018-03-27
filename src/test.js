3c838cbb0074b1048674d03ecd4178f325b4d1ed
tspeed90 <tspeed90@gmail.com>
2018-03-27 21:39:12 +0100

    added filter function and tests function

--- src/test.js
+++ src/test.js
@@ -1,14 +1,27 @@
-const logic = require("./logic");
+const { filter } = require("./logic");
 const test = require("tape");
 const supertest = require("supertest");
 const router = require("./router");
+const dummy = require('./dummy.json');
 
 test("Testing Tape is working", t => {
-    t.equal(1, 1, "Tape is not working");
+    t.equal(1, 1, "Tape is working");
     t.end();
 });
+
+
 
 
 // --- TESTING FOR SERVER STUFF
 
 // --- TESTING FOR THE LOGIC FUNCTIONS
+test("Filtered object contains 4 properties", t => {
+    const actual = filter(dummy);
+    if (Object.keys(actual[0]).length === 4) {
+        t.pass('Object should contain 4 items');
+    } else {
+        t.fail('Object should contain 4 items');
+    }
+    t.end()
+});
+
